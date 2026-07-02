f3ccff2b5ede1e8cf0fc335ead69750bae1657af
Derradji-gns <derradjiamineabdelbaset@gmail.com>
2026-07-02 23:19:56 +0100

    ts

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -31,7 +31,7 @@ const projects = [
     description:
       "A clean, focused entry point designed to capture attention and drive user action",
     tags: ["Shopify", "Remix"],
-    link: "landing-page44.vercel.app",
+    link: "https://landing-page44.vercel.app",
   },
   {
     id: 4,
